41261181bb96101d254ef4054dc6f5602a4f07db
Haroon <haroon.ahmed25@gmail.com>
2017-11-03 10:46:03 +0000

    Show question page for non auth users
    Put a nice helpful message to non authenticated users, encouraging them to sign up.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
   # GET /questions
   # GET /questions.json
