493c4809c02464a33e9724365f23a0e96ec19547
Haroon <haroon.ahmed25@gmail.com>
2017-12-11 17:29:24 +0000

    Capture errors in Errbit using airbrake gem

--- Gemfile
+++ Gemfile
@@ -65,6 +65,9 @@ gem 'sanitize'
 # A/B testing
 gem 'split', require: 'split/dashboard'
 
+# Error tracking 
+gem 'airbrake'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,9 @@
+Airbrake.configure do |config|
+  config.host = 'https://funky-errors.herokuapp.com'
+  config.project_id = 1 # required, but any positive integer works
+  config.project_key = '972afefa2eea6ff117f8d9e58beee44e'
+
+  # Uncomment for Rails apps
+  config.environment = Rails.env
+  config.ignore_environments = %w(test)
+end
